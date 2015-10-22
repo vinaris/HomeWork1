477214f1587f2a66a3317485f16c2cc828726b9d
Валерий Касперович <link206@yandex.ru>
2015-10-23 01:43:21 +0700

    OK

---
+++ HomeWork1/Model/CatColor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class CatColor
+    {
+        public string HeathyColor { get; set; } = "Белый";
+        public string SickColor { get; set; } = "Зелёный";
+    }
+}
