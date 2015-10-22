5bfe276a87e7f8cd543022066ace3f0eb0f7b2c6
Валерий Касперович <link206@yandex.ru>
2015-10-23 01:43:58 +0700

    add

---
+++ HomeWork1/Model/Cat.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Cat
+    {
+        private string _name;
+        private int _health;
+
+        public Cat(string age, CatColor color)
+        {
+            Age = age;
+            _health = 5;
+            Color = color;
+            CurrentColor = color.HeathyColor;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrEmpty(_name))
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public string Age { get; }
+
+        public CatColor Color { get; set; }
+
+        public void Feed()
+        {
+            _health++;
+        }
+
+        public void Punish()
+        {
+            _health--;
+        }
+
+        public string CurrentColor { get; set; }
+
+        public void SetCurrentColor()
+        {
+            CurrentColor = _health < 5 ? Color.SickColor : Color.HeathyColor;
+        }
+
+    }
+}
---
+++ HomeWork1/View/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите возраст кошки: ");
+            string age = Console.ReadLine();
+            var color = new CatColor();
+            var cat = new Cat(age, color);
+            while (true)
+            {
+                Console.WriteLine("Текущая информация о кошке.\nИмя: " + cat.Name + "\nВозраст: " + cat.Age + "\nТекущий цвет: " + cat.CurrentColor);
+                Console.WriteLine("Выберите действие:\n1. Задать имя\n2. Задать цвет\n3. Ударить\n4. Покормить\n5. Выход");
+                string key = Console.ReadLine();
+                switch (key)
+                {
+                    case "1":
+                        Console.Write("Введите имя кошке: ");
+                        cat.Name = Console.ReadLine();
+                        break;
+
+                    case "2":
+                        Console.Write("Введите цвет здоровой кошки: ");
+                        cat.Color.HeathyColor = Console.ReadLine();
+                        Console.Write("Введите цвет больной кошки: ");
+                        cat.Color.SickColor = Console.ReadLine();
+                        cat.SetCurrentColor();
+                        break;
+
+                    case "3":
+                        cat.Punish();
+                        cat.SetCurrentColor();
+                        break;
+
+                    case "4":
+                        cat.Feed();
+                        cat.SetCurrentColor();
+                        break;
+
+                    case "5":
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Такого пункта нет!");
+                        break;
+
+                }
+            }
+        }
+    }
+}
